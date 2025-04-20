13fd8f093abfb7974fb1ff792a3cbc6d26ddb093
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-04-20 18:13:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ client.on('message_create', async message => {
 
         if (media.mimetype === 'image/webp') {
             console.log('skip');
-            //return;
         }
         else if (media.mimetype.startsWith('image')) {
             telegramBot.sendPhoto(telegramChatId, Buffer.from(media.data, 'base64'), { caption: 'You' });
